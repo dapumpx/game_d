40260dff750ac343eb3d19c5284e1e7ddf8f8891
dapumpx <xzh.xuzhihao@gmail.com>
2018-10-25 10:42:25 +0800

    update express

--- phaser_client/app.js
+++ phaser_client/app.js
@@ -1,26 +1,33 @@
 var express = require('express');
 var app = express();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser')
+var errorhandler = require('errorhandler');
 
-app.configure(function(){
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(app.router);
-	app.set('view engine', 'jade');
-	app.set('views', __dirname + '/public');
-	app.set('view options', {layout: false});
-	app.set('basepath',__dirname + '/public');
-});
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({
+	extended: true
+  }));
+app.use(bodyParser.json());
+// app.use(app.router);
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/public');
+app.set('view options', {layout: false});
+app.set('basepath',__dirname + '/public');
 
-app.configure('development', function(){
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+switch(app.settings.env)
+{
+	case 'development':
+		app.use(express.static(__dirname + '/public'));
+		app.use(errorhandler({ dumpExceptions: true, showStack: true }));
+		break;
 
-app.configure('production', function(){
-	var oneYear = 31557600000;
-	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-	app.use(express.errorHandler());
-});
+	case 'production':
+		var oneYear = 31557600000;
+		app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+		app.use(errorhandler());
+		break;
+}
 
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
 app.listen(3001);
